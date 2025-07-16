87a7a79c0b7261ce58b2d7253cf4d258799d2a79
1franken11 <francoexequiel.garcia3@gmail.com>
2025-07-16 15:53:21 -0300

    quit carousel

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,7 @@ export { default as Navbar } from './Navbar.astro';
 export { default as QuienesSomos } from './QuienesSomos.astro';
 export { default as FlooringSection } from './FlooringSection.astro';
 export { default as ImageComparison } from './imageComparison/ImageComparison.astro';
-export { default as OpinionesCarousel } from './opinionesCarousel/OpinionesCarousel.astro';
+// export { default as OpinionesCarousel } from './opinionesCarousel/OpinionesCarousel.astro';
 export { default as ContactForm } from './ContactForm.astro';
 export { default as ShadersSliderMultilingual } from './ShadersSlider/ShadersSliderMultilingual.astro';
 export { default as ReviewsCarousel } from './ReviewsCarousel.astro';
